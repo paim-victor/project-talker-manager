a3865e1ea2fc4ade84aa74205a0912d9244675e7
Paim <victor-paim@hotmail.com>
2023-02-07 10:51:41 -0300

    arrumando lint e refatorando meu codigo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
-const { readTalkers, writeTalkers } =  require('./utils/utilTalkers');
+const { readTalkers, writeTalkers } = require('./utils/utilTalkers');
 const genToken = require('./utils/genToken');
 const authToken = require('./middlewares/authToken');
-const name  = require('./middlewares/name');
-const age  = require('./middlewares/age');
-const talk  = require('./middlewares/talk');
-const watchedAT  = require('./middlewares/watchedAt');
+const validName = require('./middlewares/validName');
+const validAge = require('./middlewares/validAge');
+const validTalk = require('./middlewares/validTalk');
+const watchedAT = require('./middlewares/watchedAt');
 const rate = require('./middlewares/rate');
+const emailANDpassword = require('./middlewares/emailANDpassword');
 
 const app = express();
 app.use(express.json());
@@ -27,7 +28,7 @@ app.listen(PORT, () => {
 app.get('/talker', async (req, res) => {
   const palestrantes = await readTalkers();
 
-  if(palestrantes) {
+  if (palestrantes) {
     return res.status(200).json(palestrantes);
   }
   return res.status(200).json([]);
@@ -35,39 +36,38 @@ app.get('/talker', async (req, res) => {
 
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
-  const palestrantes =  await readTalkers();
+  const palestrantes = await readTalkers();
 
-  const chosenTalkers = palestrantes.find((palestrante) => palestrante.id === Number(id))
+  const chosenTalkers = palestrantes.find((palestrante) => palestrante.id === Number(id));
 
-  if(chosenTalkers) {
+  if (chosenTalkers) {
     return res.status(200).json(chosenTalkers);
   }
-  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' })
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
-app.post('/login', async (req, res) => { 
-  const { email, password } = req.body;
-  const validateEmail = (email) => email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/);
-
-  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  if (!validateEmail(email)) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  if (password.length < 6) return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  
+app.post('/login', emailANDpassword, async (req, res) => {
   const token = genToken();
-  return res.status(200).json(token)
+  return res.status(200).json(token);
 });
 
-app.post('/talker', authToken, name, age, talk, rate, watchedAT, async (req, res) => {
-  const { name, age, talk } = req.body;
-  const palestrantes = await readTalkers();
-  const novoPalestrante = {
-    id: palestrantes.length + 1,
-    name,
-    age,
-    talk
-  };
-  palestrantes.push(novoPalestrante);
-  await writeTalkers([...palestrantes, req.body]);
-    return res.status(201).json(novoPalestrante);
+app.post('/talker', 
+  authToken, 
+  validName, 
+  validAge, 
+  validTalk, 
+  rate, 
+  watchedAT, 
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+    const palestrantes = await readTalkers();
+    const novoPalestrante = {
+      id: palestrantes.length + 1,
+      name,
+      age,
+      talk,
+    };
+    palestrantes.push(novoPalestrante);
+    await writeTalkers([...palestrantes, req.body]);
+      return res.status(201).json(novoPalestrante);
 });
